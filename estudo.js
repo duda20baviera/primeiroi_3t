a726e596a886fab426a7215498c9409c019ec42a
duda20baviera <110567880+duda20baviera@users.noreply.github.com>
2022-10-11 18:13:26 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -59,3 +59,11 @@ function média(){
     document.getElementById("resultado").innerHTML = r;
 
 }
+    function necessário(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = 240 - (Number(n1) + Number(n2) + Number(n3) + Number(n4));
+    document.getElementById("resultado").innerHTML = r;
+}
