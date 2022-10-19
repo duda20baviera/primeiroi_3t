8dcc9d2b633f2ebea7da86779dca79416ee0c1f8
duda20baviera <110567880+duda20baviera@users.noreply.github.com>
2022-10-19 17:41:47 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -35,49 +35,13 @@ function quadrado(){
 function total(){
     let val = document.getElementById("valor").value;
     let ju = document.getElementById("juros").value;
-    let mes = document.getElementById("mes").value;
-
-    if(!Number (val)){
-        alert("O valor deve ser um número");
-        document.getElementById("valor"). value = "";
-        document.getElementById("valor"). focus = ();
-        return
-    } 
-
-
-   let resultado = 0;
-   for(let i = 1; i <= mes; i++){
-       resultado = valor * (1+(iuros/100)):
-       valor = resultado;
-   }
-
-   document.write ("O Resultado é " + resultado);
-
-}
-
-function soma(){
-    let n1 = document.getElementById("b1").value;
-    let n2 = document.getElementById("b2").value;
-    let n3 = document.getElementById("b3").value;
-    let n4 = document.getElementById("b4").value;
-    let r = Number(n1) + Number(n2) + Number(n3) + Number(n4);
-    document.getElementById("resultado").innerHTML = r;
-}
-
-function média(){
-    let n1 = document.getElementById("b1").value;
-    let n2 = document.getElementById("b2").value;
-    let n3 = document.getElementById("b3").value;
-    let n4 = document.getElementById("b4").value;
-    let r = Number(n1) + Number(n2) + Number(n3) + Number(n4))/4;
-    document.getElementById("resultado").innerHTML = r;
-}
-
-function necessário(){
-    let n1 = document.getElementById("b1").value;
-    let n2 = document.getElementById("b2").value;
-    let n3 = document.getElementById("b3").value;
-    let n4 = document.getElementById("b4").value;
-    let r = 240 - Number(n1) + Number(n2) + Number(n3) + Number(n4);
-    document.getElementById("resultado").innerHTML = r;
+    
+    let t = document.getElementById("meses").value;
+    let resultado = 0;
+    for(let m = 1; m <= t; m++){
+        resultado = (val * ((ju/100)+1));
+        val = resultado;
+        document.write("Mês " + m + " valor de " + val + "<br>");
+    }
+    document.write("O total é de: " + resultado);
 }
